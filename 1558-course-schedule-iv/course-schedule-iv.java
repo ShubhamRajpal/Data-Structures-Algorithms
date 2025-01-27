24bbb05fc653f5571de702de2fbde05459c01fb5
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-01-27 13:33:20 +0530

    Time: 68 ms (30.00%) | Memory: 47.7 MB (74.12%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.java
@@ -0,0 +1,63 @@
+class Solution {
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        List<List<Integer>> adjList = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++){
+            adjList.add(new ArrayList<>());
+        }
+
+        int indegree[] = new int[numCourses];
+        for(int i = 0; i < prerequisites.length; i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adjList.get(u).add(v);
+            indegree[v]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        Set<Integer> uniqueNodes = new HashSet<>();
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0) {
+                q.offer(i);
+                uniqueNodes.add(i);
+            }
+        }
+
+        int i = 0;
+        Map<Integer, Set<Integer>> prereqNodes = new HashMap<>();
+        while(!q.isEmpty()){
+            int node = q.poll();
+
+            for(int adjNode : adjList.get(node)){
+                
+                if(prereqNodes.get(adjNode) == null){
+                    Set<Integer> temp = new HashSet<>();
+                    temp.add(node);
+                    prereqNodes.put(adjNode, temp);
+                }
+                prereqNodes.get(adjNode).add(node);
+                for(int curPrereq : prereqNodes.getOrDefault(node, new HashSet<>())){
+                    prereqNodes.get(adjNode).add(curPrereq);
+                }
+
+                indegree[adjNode]--;
+                if(indegree[adjNode] == 0) q.offer(adjNode);
+            }
+        }
+
+        List<Boolean> ans = new ArrayList<>();
+
+        // System.out.println(topo);
+        // System.out.println(uniqueNodes);
+        for(int j = 0; j < queries.length; j++){
+            int u = queries[j][0];
+            int v = queries[j][1];
+
+            if(prereqNodes.getOrDefault(v, new HashSet<>()).contains(u)){
+                ans.add(true);
+            }else ans.add(false);
+        }
+
+        return ans;
+
+    }
+}
