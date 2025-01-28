533f479726faa29446161b1d1a4a8ec85dde6229
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-01-28 11:34:32 +0530

    Time: 17 ms (10.36%) | Memory: 52.8 MB (5.04%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
@@ -1,3 +1,11 @@
+class Pair{
+    int first, second;
+    public Pair(int f, int s){
+        first = f;
+        second = s;
+    }
+}
+
 class Solution {
 
     private int dfs(int i, int j, int n, int m, int[][] grid){
@@ -17,6 +25,38 @@ class Solution {
 
         return totalFishCount;
     }
+
+    private int bfs(int i, int j, int n, int m, int[][] grid){
+
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i, j));
+
+        int totalFishCount = 0;
+        int[] dx = {-1,0,1,0};
+        int[] dy = {0,1,0,-1};
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            int row = p.first;
+            int col = p.second;
+            
+            totalFishCount += grid[row][col];
+            grid[row][col] = 0;
+            
+            for(int it=0;it<4;it++){
+                int nrow = row+dx[it];
+                int ncol = col+dy[it];
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] > 0){
+                    q.add(new Pair(nrow, ncol));
+                }
+            }
+
+        }
+
+        return totalFishCount;
+
+    }
+
+
     public int findMaxFish(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
@@ -25,7 +65,8 @@ class Solution {
         for(int i = 0; i < n; i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]>0){
-                    maxFishCount = Math.max(maxFishCount, dfs(i,j,n,m,grid));
+                    // maxFishCount = Math.max(maxFishCount, dfs(i,j,n,m,grid));
+                    maxFishCount = Math.max(maxFishCount, bfs(i,j,n,m,grid));
                 }
             }
         }
