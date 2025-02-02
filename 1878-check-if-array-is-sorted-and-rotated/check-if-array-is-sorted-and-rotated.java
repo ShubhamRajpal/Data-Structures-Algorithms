1012f174b567209f0a6c7a53bfdd1d30e4aa6975
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-02-02 12:24:08 +0530

    Time: 1 ms (5.83%) | Memory: 41.4 MB (38.09%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,31 +1,50 @@
 class Solution {
     public boolean check(int[] nums) {
         int n = nums.length;
-        int[] sorted = new int[n];
+        // int[] sorted = new int[n];
+        // for(int r = 0; r <= n; r++){
+        //     int idx = 0;
+        //     for(int i = r; i < n; i++){
+        //         sorted[idx] = nums[i];
+        //         idx++;
+        //     }
+
+        //     for(int  i = 0; i < r; i++){
+        //         sorted[idx] = nums[i];
+        //         idx++;
+        //     }
+
+        //     boolean isSorted = true;
+        //     for(int i = 0; i < n-1; i++){
+        //         if(sorted[i] > sorted[i+1]){
+        //             isSorted = false;
+        //             break;
+        //         }
+        //     }
+
+        //     if(isSorted) return true;
+
+        // }
+
+        // return false;
+
+        // Better
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
+
         for(int r = 0; r <= n; r++){
-            int idx = 0;
-            for(int i = r; i < n; i++){
-                sorted[idx] = nums[i];
-                idx++;
-            }
-
-            for(int  i = 0; i < r; i++){
-                sorted[idx] = nums[i];
-                idx++;
-            }
-
             boolean isSorted = true;
-            for(int i = 0; i < n-1; i++){
-                if(sorted[i] > sorted[i+1]){
+            for(int i = 0; i < n; i++){
+                if(sorted[i] != nums[(i+r)%n]){
                     isSorted = false;
                     break;
                 }
+
             }
-
             if(isSorted) return true;
-
         }
 
         return false;
+
     }
 }
