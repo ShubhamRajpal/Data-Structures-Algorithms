5f910b9d80e4eec808e274ced0e352dd1ffa35ac
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-02-06 14:46:07 +0530

    Time: 212 ms (62.82%) | Memory: 68.8 MB (59.62%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.java
+++ 1364-tuple-with-same-product/tuple-with-same-product.java
@@ -23,27 +23,48 @@ class Solution {
         // }
         // return tupleCnt*8;
 
-        // optimal
-        int n = nums.length;
-        Arrays.sort(nums);
-        int tuple = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = n - 1; j > i; j--) {
-                int p1 = nums[i] * nums[j];
-                Set<Integer> st = new HashSet<>();
+        // Better
+        // int n = nums.length;
+        // Arrays.sort(nums);
+        // int tuple = 0;
+        // for (int i = 0; i < n; i++) {
+        // for (int j = n - 1; j > i; j--) {
+        // int p1 = nums[i] * nums[j];
+        // Set<Integer> st = new HashSet<>();
 
-                for (int k = i + 1; k < j; k++) {
-                    if (p1 % nums[k] == 0) {
-                        int l = p1 / nums[k];
-                        if (st.contains(l)) {
-                            tuple++;
-                        }
-                        st.add(nums[k]);
-                    }
-                }
+        // for (int k = i + 1; k < j; k++) {
+        // if (p1 % nums[k] == 0) {
+        // int l = p1 / nums[k];
+        // if (st.contains(l)) {
+        // tuple++;
+        // }
+        // st.add(nums[k]);
+        // }
+        // }
+        // }
+        // }
+
+        // return tuple * 8;
+
+        // Optimal
+        int tuple = 0;
+        int n = nums.length;
+        Map<Integer, Integer> prodMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int p = nums[i] * nums[j];
+                prodMap.put(p, prodMap.getOrDefault(p, 0) + 1);
             }
         }
 
-        return tuple * 8;
+        // Find if a product occurs more than once
+        for(Map.Entry<Integer, Integer> entry : prodMap.entrySet()){
+            if(entry.getValue() > 1){
+                int val = entry.getValue();
+                tuple += (val * (val-1))/2;
+            }
+        }
+
+        return tuple*8;
     }
 }
