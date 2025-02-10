dcfd134cf645daabb89610275ed4032147032bd7
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-02-10 12:54:59 +0530

    Time: 3 ms (38.21%) | Memory: 42.7 MB (45.64%) - LeetSync

--- 3447-clear-digits/clear-digits.java
+++ 3447-clear-digits/clear-digits.java
@@ -1,21 +1,41 @@
 class Solution {
     public String clearDigits(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        int idx = 0, n = sb.length();
-        while(idx < sb.length()){
-            if(Character.isDigit(sb.charAt(idx))){
-                sb.deleteCharAt(idx);
+        // StringBuilder sb = new StringBuilder(s);
+        // int idx = 0; 
+        // while(idx < sb.length()){
+        //     if(Character.isDigit(sb.charAt(idx))){
+        //         sb.deleteCharAt(idx);
 
-                if(idx > 0){
-                    sb.deleteCharAt(idx-1);
-                    idx--;
-                }
+        //         if(idx > 0){
+        //             sb.deleteCharAt(idx-1);
+        //             idx--;
+        //         }
 
-                if(sb.length() == 0) return "";
+        //         if(sb.length() == 0) return "";
+        //     }
+        //     else idx++;
+        // }
+
+        // return sb.toString();
+
+        // Using Stack
+        Stack<Character> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        int idx = 0 ;
+        while(idx < s.length()){
+            char ch = s.charAt(idx);
+            if(Character.isDigit(ch) && !st.isEmpty()){
+                st.pop();
+            }else{
+                st.push(ch);
             }
-            else idx++;
+            idx++;
         }
 
-        return sb.toString();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+
+        return sb.reverse().toString();
     }
 }
