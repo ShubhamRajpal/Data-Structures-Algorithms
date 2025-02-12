77c86283436d29c78093b84cd52164f2099b6c08
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-02-12 13:42:12 +0530

    Update max-sum-of-a-pair-with-equal-sum-of-digits.java

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -17,45 +17,45 @@ class Solution {
         // int maxSum = Integer.MIN_VALUE;
 
         // // Brute
-        // for (int i = 0; i < n; i++) {
-        // int dSum1 = findDigitsSum(nums[i]);
-        // for (int j = i + 1; j < n; j++) {
-        // int dSum2 = findDigitsSum(nums[j]);
-        // if (dSum1 == dSum2) {
-        // maxSum = Math.max(maxSum, nums[i] + nums[j]);
-        // }
-        // }
-        // }
+        for (int i = 0; i < n; i++) {
+            int dSum1 = findDigitsSum(nums[i]);
+            for (int j = i + 1; j < n; j++) {
+                int dSum2 = findDigitsSum(nums[j]);
+                if (dSum1 == dSum2) {
+                    maxSum = Math.max(maxSum, nums[i] + nums[j]);
+                }
+            }
+        }
 
-        // return maxSum == Integer.MIN_VALUE ? -1 : maxSum;
+        return maxSum == Integer.MIN_VALUE ? -1 : maxSum;
 
         // Better
-        // int maxSum = Integer.MIN_VALUE;
-        // // PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-        // Map<Integer, PriorityQueue<Integer>> maxIndexMap = new HashMap<>();
-        // for (int i = 0; i < n; i++) {
-        //     int digitSum = findDigitsSum(nums[i]);
-        //     PriorityQueue<Integer> temp = maxIndexMap.getOrDefault(digitSum, new PriorityQueue<Integer>());
-        //     temp.offer(nums[i]);
-        //     maxIndexMap.put(digitSum, temp);
+        int maxSum = Integer.MIN_VALUE;
+        // PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        Map<Integer, PriorityQueue<Integer>> maxIndexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int digitSum = findDigitsSum(nums[i]);
+            PriorityQueue<Integer> temp = maxIndexMap.getOrDefault(digitSum, new PriorityQueue<Integer>());
+            temp.offer(nums[i]);
+            maxIndexMap.put(digitSum, temp);
 
-        //     // We want top 2 greatest elements for each digit sum
-        //     if (maxIndexMap.get(digitSum).size() > 2) {
-        //         maxIndexMap.get(digitSum).poll();
-        //     }
-        // }
+            // We want top 2 greatest elements for each digit sum
+            if (maxIndexMap.get(digitSum).size() > 2) {
+                maxIndexMap.get(digitSum).poll();
+            }
+        }
 
-        // for (Map.Entry<Integer, PriorityQueue<Integer>> mp : maxIndexMap.entrySet()) {
-        //     int digitSum = mp.getKey();
-        //     PriorityQueue<Integer> p = mp.getValue();
-        //     if (p.size() == 2) {
-        //         int first = p.poll();
-        //         int second = p.poll();
-        //         maxSum = Math.max(maxSum, first + second);
-        //     }
-        // }
+        for (Map.Entry<Integer, PriorityQueue<Integer>> mp : maxIndexMap.entrySet()) {
+            int digitSum = mp.getKey();
+            PriorityQueue<Integer> p = mp.getValue();
+            if (p.size() == 2) {
+                int first = p.poll();
+                int second = p.poll();
+                maxSum = Math.max(maxSum, first + second);
+            }
+        }
 
-        // return maxSum == Integer.MIN_VALUE ? -1 : maxSum;
+        return maxSum == Integer.MIN_VALUE ? -1 : maxSum;
 
         // Optimal
         int[] digitSum = new int[82];
