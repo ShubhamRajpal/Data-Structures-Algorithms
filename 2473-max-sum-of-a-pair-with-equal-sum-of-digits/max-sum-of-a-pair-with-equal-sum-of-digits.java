b11e7763dfd8d4ee062e8fb62853211aaa0559a1
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-02-12 13:36:09 +0530

    Time: 22 ms (90.86%) | Memory: 55.1 MB (96.49%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,11 +1,3 @@
-class Pair{
-    int first, second;
-    public Pair(int f, int s){
-        first = f;
-        second = s;
-    }
-}
-
 class Solution {
 
     private int findDigitsSum(int num) {
@@ -26,45 +18,57 @@ class Solution {
 
         // // Brute
         // for (int i = 0; i < n; i++) {
-        //     int dSum1 = findDigitsSum(nums[i]);
-        //     for (int j = i + 1; j < n; j++) {
-        //         int dSum2 = findDigitsSum(nums[j]);
-        //         if (dSum1 == dSum2) {
-        //             maxSum = Math.max(maxSum, nums[i] + nums[j]);
-        //         }
-        //     }
+        // int dSum1 = findDigitsSum(nums[i]);
+        // for (int j = i + 1; j < n; j++) {
+        // int dSum2 = findDigitsSum(nums[j]);
+        // if (dSum1 == dSum2) {
+        // maxSum = Math.max(maxSum, nums[i] + nums[j]);
+        // }
+        // }
         // }
 
         // return maxSum == Integer.MIN_VALUE ? -1 : maxSum;
 
         // Better
-        // int[] digitSum = new int[n];
-        int maxSum = Integer.MIN_VALUE;
-        // PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-        Map<Integer, PriorityQueue<Integer>> maxIndexMap = new HashMap<>();
+        // int maxSum = Integer.MIN_VALUE;
+        // // PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        // Map<Integer, PriorityQueue<Integer>> maxIndexMap = new HashMap<>();
+        // for (int i = 0; i < n; i++) {
+        //     int digitSum = findDigitsSum(nums[i]);
+        //     PriorityQueue<Integer> temp = maxIndexMap.getOrDefault(digitSum, new PriorityQueue<Integer>());
+        //     temp.offer(nums[i]);
+        //     maxIndexMap.put(digitSum, temp);
+
+        //     // We want top 2 greatest elements for each digit sum
+        //     if (maxIndexMap.get(digitSum).size() > 2) {
+        //         maxIndexMap.get(digitSum).poll();
+        //     }
+        // }
+
+        // for (Map.Entry<Integer, PriorityQueue<Integer>> mp : maxIndexMap.entrySet()) {
+        //     int digitSum = mp.getKey();
+        //     PriorityQueue<Integer> p = mp.getValue();
+        //     if (p.size() == 2) {
+        //         int first = p.poll();
+        //         int second = p.poll();
+        //         maxSum = Math.max(maxSum, first + second);
+        //     }
+        // }
+
+        // return maxSum == Integer.MIN_VALUE ? -1 : maxSum;
+
+        // Optimal
+        int[] digitSum = new int[82];
+        Arrays.fill(digitSum, -1);
+        int maxSum = -1;
         for (int i = 0; i < n; i++) {
-            int digitSum = findDigitsSum(nums[i]);
-            PriorityQueue<Integer> temp =  maxIndexMap.getOrDefault(digitSum, new PriorityQueue<Integer>());
-            temp.offer(nums[i]);
-            maxIndexMap.put(digitSum, temp);
-            if(maxIndexMap.get(digitSum).size() > 2){
-                maxIndexMap.get(digitSum).poll();
+            int dSum = findDigitsSum(nums[i]);
+            if (digitSum[dSum] != -1) {
+                maxSum = Math.max(maxSum, nums[i] + digitSum[dSum]);
             }
-        }
-        
-
-        for (Map.Entry<Integer, PriorityQueue<Integer>> mp : maxIndexMap.entrySet()) {
-            int digitSum = mp.getKey();
-            PriorityQueue<Integer> p = mp.getValue();
-            if(p.size() == 2){
-                int first = p.poll();
-                int second = p.poll();
-                maxSum = Math.max(maxSum, first+second);
-            }
-            
-            // System.out.println(digitSum +" "+ first+" "+second);
+            digitSum[dSum] = Math.max(digitSum[dSum], nums[i]);
         }
 
-        return maxSum == Integer.MIN_VALUE ? -1 : maxSum;
+        return maxSum;
     }
 }
