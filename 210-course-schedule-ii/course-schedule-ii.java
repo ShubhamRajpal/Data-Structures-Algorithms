a7f3b140a2b694dd8cd21d5b82d2eaba33992a59
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-02-13 15:51:29 +0530

    Update course-schedule-ii.java

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -30,30 +30,30 @@ class Solution {
 
 
         // // BFS
-        // int ans[] = new int[numCourses];
-        // int[] indegree = new int[numCourses];
-        // for(int i = 0; i < prerequisites.length; i++){
-        //     indegree[prerequisites[i][0]]++; 
-        // }
+        int ans[] = new int[numCourses];
+        int[] indegree = new int[numCourses];
+        for(int i = 0; i < prerequisites.length; i++){
+            indegree[prerequisites[i][0]]++; 
+        }
 
-        // Queue<Integer> q = new LinkedList<>();
-        // for(int i = 0; i < numCourses; i++){
-        //     if(indegree[i] == 0) q.add(i);
-        // }
-        // int cnt = 0, i= 0;
-        // while(!q.isEmpty()){
-        //     int front = q.poll();
-        //     cnt++;
-        //     ans[i++] = front;
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0) q.add(i);
+        }
+        int cnt = 0, i= 0;
+        while(!q.isEmpty()){
+            int front = q.poll();
+            cnt++;
+            ans[i++] = front;
 
-        //     for(int it : adj.get(front)){
-        //         indegree[it]--;
-        //         if(indegree[it] == 0) q.add(it);
-        //     }
-        // }
+            for(int it : adj.get(front)){
+                indegree[it]--;
+                if(indegree[it] == 0) q.add(it);
+            }
+        }
 
-        // if(i == numCourses) return ans;
-        // return new int[0];
+        if(i == numCourses) return ans;
+        return new int[0];
 
         // DFS
         Stack<Integer> st = new Stack<>();
