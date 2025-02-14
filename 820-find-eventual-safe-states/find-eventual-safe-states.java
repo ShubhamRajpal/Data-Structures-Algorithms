336796d1473803e976c236a09082ee8c586a61b9
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-02-14 15:22:45 +0530

    Time: 15 ms (45.47%) | Memory: 55 MB (97.54%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -23,38 +23,42 @@ class Solution {
 
         // BFS
         // List<List<Integer>> adjRev = new ArrayList<>();
-        // for(int i = 0; i < V; i++){
-        // adjRev.add(new ArrayList<>());
+        // for (int i = 0; i < V; i++) {
+        //     adjRev.add(new ArrayList<>());
         // }
         // int[] indegree = new int[V];
         // int[] safe = new int[V];
 
-        // for(int i = 0; i < V; i++){
-        // for(int it : adj[i]){
-        // adjRev.get(it).add(i);
-        // indegree[i]++;
-        // }
+        // for (int i = 0; i < V; i++) {
+        //     for (int it : adj[i]) {
+        //         adjRev.get(it).add(i);
+        //         indegree[i]++;
+        //     }
         // }
 
         // List<Integer> topo = new ArrayList<>();
 
         // Queue<Integer> q = new LinkedList<Integer>();
 
-        // for(int i = 0; i < V; i++){
-        // if(indegree[i] == 0) q.add(i);
+        // for (int i = 0; i < V; i++) {
+        //     if (indegree[i] == 0)
+        //         q.add(i);
         // }
 
-        // while(!q.isEmpty()){
-        // int node = q.poll();
-        // // topo.add(node);
-        // safe[node] = 1;
-        // for(int it : adjRev.get(node)){
-        // indegree[it]--;
-        // if(indegree[it] == 0) q.add(it);
-        // }
+        // while (!q.isEmpty()) {
+        //     int node = q.poll();
+        //     // topo.add(node);
+        //     safe[node] = 1;
+        //     for (int it : adjRev.get(node)) {
+        //         indegree[it]--;
+        //         if (indegree[it] == 0)
+        //             q.add(it);
+        //     }
         // }
 
-        // for(int i = 0; i < V; i++) if(safe[i] == 1) topo.add(i);
+        // for (int i = 0; i < V; i++)
+        //     if (safe[i] == 1)
+        //         topo.add(i);
         // return topo;
 
         // DFS
