47bc24a23bc5eccbd362addd9e55d51ffde1a9b3
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-02-14 15:50:49 +0530

    Update find-eventual-safe-states.java

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -22,44 +22,44 @@ class Solution {
         int V = adj.length;
 
         // BFS
-        // List<List<Integer>> adjRev = new ArrayList<>();
-        // for (int i = 0; i < V; i++) {
-        //     adjRev.add(new ArrayList<>());
-        // }
-        // int[] indegree = new int[V];
-        // int[] safe = new int[V];
+        List<List<Integer>> adjRev = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adjRev.add(new ArrayList<>());
+        }
+        int[] indegree = new int[V];
+        int[] safe = new int[V];
 
-        // for (int i = 0; i < V; i++) {
-        //     for (int it : adj[i]) {
-        //         adjRev.get(it).add(i);
-        //         indegree[i]++;
-        //     }
-        // }
+        for (int i = 0; i < V; i++) {
+            for (int it : adj[i]) {
+                adjRev.get(it).add(i);
+                indegree[i]++;
+            }
+        }
 
-        // List<Integer> topo = new ArrayList<>();
+        List<Integer> topo = new ArrayList<>();
 
-        // Queue<Integer> q = new LinkedList<Integer>();
+        Queue<Integer> q = new LinkedList<Integer>();
 
-        // for (int i = 0; i < V; i++) {
-        //     if (indegree[i] == 0)
-        //         q.add(i);
-        // }
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0)
+                q.add(i);
+        }
 
-        // while (!q.isEmpty()) {
-        //     int node = q.poll();
-        //     // topo.add(node);
-        //     safe[node] = 1;
-        //     for (int it : adjRev.get(node)) {
-        //         indegree[it]--;
-        //         if (indegree[it] == 0)
-        //             q.add(it);
-        //     }
-        // }
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            // topo.add(node);
+            safe[node] = 1;
+            for (int it : adjRev.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.add(it);
+            }
+        }
 
-        // for (int i = 0; i < V; i++)
-        //     if (safe[i] == 1)
-        //         topo.add(i);
-        // return topo;
+        for (int i = 0; i < V; i++)
+            if (safe[i] == 1)
+                topo.add(i);
+        return topo;
 
         // DFS
         List<List<Integer>> adjList = new ArrayList<>();
