dff82486361f6c5c1ef4d2489ed43bdb1c59100b
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-02-15 10:20:52 +0530

    Time: 166 ms (21.94%) | Memory: 44.6 MB (44.51%) - LeetSync

---
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    private boolean backtrack(int num, String square, int curSum) {
+        if (square.length() == 0) {
+            return curSum == num;
+        }
+
+        for (int i = 0; i < square.length(); i++) {
+            if (backtrack(num, square.substring(i + 1), Integer.valueOf(square.substring(0, i + 1)) + curSum))
+                return true;
+        }
+
+        return false;
+    }
+
+    public int punishmentNumber(int n) {
+        
+        int sum = 0;
+        for(int i = 1; i <= n ; i++){
+            if(backtrack(i, Integer.toString(i*i), 0)){
+                sum += i*i;
+            }
+        }
+
+        return sum;
+    }
+}
