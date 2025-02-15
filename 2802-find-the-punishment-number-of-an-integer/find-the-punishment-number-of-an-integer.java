cbc0cd764a9addf0a4d7d474514b6e8eb9e74094
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-02-15 11:48:47 +0530

    Time: 274 ms (18.81%) | Memory: 44.9 MB (17.55%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.java
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.java
@@ -1,27 +1,69 @@
 class Solution {
 
-    private boolean backtrack(int num, String square, int curSum) {
+    private boolean check(int num, String square, int curSum) {
         if (square.length() == 0) {
             return curSum == num;
         }
 
         for (int i = 0; i < square.length(); i++) {
-            if (backtrack(num, square.substring(i + 1), Integer.parseInt(square.substring(0, i + 1)) + curSum))
+            if (check(num, square.substring(i + 1), Integer.parseInt(square.substring(0, i + 1)) + curSum))
                 return true;
         }
 
         return false;
     }
 
-    public int punishmentNumber(int n) {
-        
-        int sum = 0;
-        for(int i = 1; i <= n ; i++){
-            if(backtrack(i, Integer.toString(i*i), 0)){
-                sum += i*i;
+    private boolean checkMemo(int i, int num, String sq, int curSum, int [][] memo){
+        if(i == sq.length()){
+            return curSum == num;
+        }
+
+        if(curSum > num) return false;
+
+        if(memo[i][curSum] != -1) return memo[i][curSum] == 1;
+
+
+        boolean possible = false;
+        for(int j = i; j < sq.length(); j++){
+            String str = sq.substring(i, j+1);
+            int val = Integer.parseInt(str);
+
+            possible = possible || checkMemo(j+1, num, sq, curSum + val, memo);
+
+            if(possible) {
+                memo[i][curSum] = 1;
+                return true;
             }
         }
 
-        return sum;
+        memo[i][curSum] = 0;
+        return false;
+    }
+
+    public int punishmentNumber(int n) {
+        
+        // int sum = 0;
+        // for(int i = 1; i <= n ; i++){
+        //     if(check(i, Integer.toString(i*i), 0)){
+        //         sum += i*i;
+        //     }
+        // }
+
+        // return sum;
+
+        // Memoization
+        int res = 0;
+        for(int i = 1; i <= n ; i++){
+            String sq = Integer.toString(i*i);
+
+            int[][] memo = new int[sq.length()+1][i+1];
+            for(int it[] : memo) Arrays.fill(it, -1);
+
+            if(checkMemo(0, i, sq, 0, memo)){
+                res += Integer.valueOf(sq);
+            }
+        }
+
+        return res;
     }
 }
