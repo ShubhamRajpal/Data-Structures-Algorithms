8be4081f06bf916164a90e5f628821850b39f666
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-02-16 12:09:30 +0530

    Time: 62 ms (100.00%) | Memory: 80.9 MB (100.00%) - LeetSync

---
+++ 3779-eat-pizzas/eat-pizzas.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long maxWeight(int[] pizzas) {
+        int n = pizzas.length;
+        Arrays.sort(pizzas);
+        int totalDays = n/4;
+        int evenCount = totalDays/2;
+        int oddCount = totalDays - evenCount;
+
+        int right = n-1;
+        long maxWeight = 0;
+        for(int i = 0; i < oddCount; i++){
+            maxWeight += pizzas[right--];
+        }
+
+        right--;
+        if(right >= 0){
+            for(int i = 0; i < evenCount; i++){
+                if(right <= 0) break;
+                maxWeight += pizzas[right];
+                right -= 2;
+            }
+        }
+
+        return maxWeight;
+        
+    }
+}
