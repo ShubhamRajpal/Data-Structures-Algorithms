0f59820b7a07cede3e9c88407982215bf8021eb8
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-02-16 10:03:12 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (100.00%) - LeetSync

--- 3709-find-special-substring-of-length-k/find-special-substring-of-length-k.java
+++ 3709-find-special-substring-of-length-k/find-special-substring-of-length-k.java
@@ -1,23 +1,48 @@
 class Solution {
     public boolean hasSpecialSubstring(String s, int k) {
-        int n = s.length();
-        char str[] = s.toCharArray();
-        int cnt = 0;
+        // int n = s.length();
+        // char str[] = s.toCharArray();
 
-        for (int i = 0; i <= n - k; i++) {
-            char ch = str[i];
-            boolean flag = true;
-            for (int j = i; j < i + k; j++) {
-                if(str[j] != ch){
-                    flag = false;
-                    break;
+        // for (int i = 0; i <= n - k; i++) {
+        //     char ch = str[i];
+        //     boolean flag = true;
+        //     for (int j = i; j < i + k; j++) {
+        //         if (str[j] != ch) {
+        //             flag = false;
+        //             break;
+        //         }
+        //     }
+
+        //     if (flag) {
+        //         if ((i > 0 && str[i - 1] == ch) || (i + k < n && str[i + k] == str[i]))
+        //             continue;
+        //         return true;
+        //     }
+        // }
+
+        // return false;
+
+        // Sliding Window
+        int low = 0, high = 0, n = s.length();
+        char str[] = s.toCharArray();
+        int[] freq = new int[26];
+
+        while (high < n) {
+            freq[str[high] - 'a']++;
+
+            while(high - low + 1 > k){
+                freq[str[low] - 'a']--;
+                low++;
+            }
+
+            if (high - low + 1 == k) {
+                if (freq[str[low] - 'a'] == k && (low == 0 || str[low - 1] != str[low]) &&
+                        (high == n-1 || str[high] != str[high + 1])) {
+                    return true;
                 }
             }
 
-            if(flag){
-                if((i > 0 && str[i-1] == ch) || (i+k < n && str[i+k] == str[i])) continue;
-                return true;
-            }
+            high++;
         }
 
         return false;
