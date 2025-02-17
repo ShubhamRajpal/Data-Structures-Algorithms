50cf6dade066ef5f5236ff1d0b357decf452d011
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-02-17 13:24:49 +0530

    Time: 2 ms (87.75%) | Memory: 41.4 MB (85.91%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.java
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.java
@@ -1,11 +1,12 @@
 class Solution {
 
-    private void solve(String tiles, StringBuilder cur, Set<String> res, boolean[] used){
+    private void solve(String tiles, StringBuilder cur, Set<String> res, boolean[] used) {
         res.add(cur.toString());
 
-        for(int i =0;i< tiles.length(); i++){
-            if(used[i] == true) continue;
-            
+        for (int i = 0; i < tiles.length(); i++) {
+            if (used[i] == true)
+                continue;
+
             used[i] = true;
             cur.append(tiles.charAt(i));
 
@@ -16,16 +17,43 @@ class Solution {
         }
     }
 
+     private int solve(int[] freq) {
+        
+
+        int cnt  = 0;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0) continue;
+
+            freq[i]--;
+            cnt++;
+            cnt  += solve(freq);
+            freq[i]++;
+        }
+
+        return cnt;
+    }
+
 
     public int numTilePossibilities(String tiles) {
-        int n = tiles.length();
+        // int n = tiles.length();
 
-        Set<String> res = new HashSet<>();
-        boolean[] used = new boolean[n];
-        StringBuilder cur = new StringBuilder();
+        // Set<String> res = new HashSet<>();
+        // boolean[] used = new boolean[n];
+        // StringBuilder cur = new StringBuilder();
 
-        solve(tiles, cur, res, used);
+        // solve(tiles, cur, res, used);
 
-        return res.size() - 1;
+        // return res.size() - 1;
+
+        // Approach 2
+        int[] freq = new int[26];
+        for(char ch : tiles.toCharArray()){
+            freq[ch - 'A']++;
+        }
+
+        int count = 0;
+        return solve(freq);
+
+        
     }
 }
