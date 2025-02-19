25fe1ad483594e59280e5aa26da4754686bc3f29
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-02-19 13:19:54 +0530

    Time: 7 ms (68.56%) | Memory: 43.1 MB (66.06%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,32 +1,30 @@
 class Solution {
 
-    private void solve(StringBuilder sb, char prev, int n, int k, List<String> ans){
-        if(sb.length() == n){
+    private void solve(StringBuilder sb, int n, int k, List<String> ans) {
+        if (sb.length() == n) {
             ans.add(sb.toString());
             return;
         }
 
-        for(int i = 0; i < 3; i++){
-            
-            if(ans.size() == k) break;
+        for (char curChar = 'a'; curChar <= 'c'; curChar++) {
 
-            char curChar = (char)('a'+i);  
-            if(prev == '\0' || curChar != prev){
+            if (ans.size() == k) break;
+
+            if (sb.length() == 0 || curChar != sb.charAt(sb.length() - 1)) {
                 sb.append(curChar);
-                solve(sb, curChar, n, k, ans);
+                solve(sb, n, k, ans);
                 sb.deleteCharAt(sb.length() - 1);
             }
         }
     }
 
-
     public String getHappyString(int n, int k) {
         List<String> ans = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-         
-        solve(sb, '\0', n, k, ans);
+
+        solve(sb, n, k, ans);
         System.out.print(ans);
-        return ans.size() < k ? "" : ans.get(k-1);
+        return ans.size() < k ? "" : ans.get(k - 1);
 
     }
 }
