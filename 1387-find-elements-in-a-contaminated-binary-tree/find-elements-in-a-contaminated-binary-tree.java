8738fc25cf5a05be08fd43757a80ae7d3e572f0a
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-02-21 12:01:53 +0530

    Time: 25 ms (30.19%) | Memory: 47.2 MB (72.24%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.java
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.java
@@ -1,50 +1,81 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class FindElements {
     public TreeNode temp;
     Set<Integer> present;
+
     public FindElements(TreeNode root) {
         temp = root;
         present = new HashSet<>();
 
-        //DFS
-        temp.val = 0;
-        present.add(0);
-        dfsRecover(temp);
+        // DFS
+        // temp.val = 0;
+        // present.add(0);
+        // dfsRecover(temp);
+
+        // BFS
+        bfsRecover(temp);
     }
-    
+
     public boolean find(int target) {
-         return present.contains(target);
+        return present.contains(target);
 
     }
 
-    public void dfsRecover(TreeNode node){
-        if(node == null)  return;
+    public void dfsRecover(TreeNode node) {
+        if (node == null)
+            return;
 
-        if(node.left != null){
+        if (node.left != null) {
             node.left.val = node.val * 2 + 1;
             present.add(node.left.val);
             dfsRecover(node.left);
         }
 
-        if(node.right != null){
+        if (node.right != null) {
             node.right.val = node.val * 2 + 2;
             present.add(node.right.val);
             dfsRecover(node.right);
         }
+    }
+
+    public void bfsRecover(TreeNode root) {
+
+        if(root == null) return;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        root.val = 0;
+        q.add(root);
+        
+
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            present.add(node.val);
+
+            if (node.left != null) {
+                node.left.val = node.val * 2 + 1;
+                q.add(node.left);
+            }
+
+            if (node.right != null) {
+                node.right.val = node.val * 2 + 2;
+                q.add(node.right);
+            }
+        }
+
     }
 
 }
