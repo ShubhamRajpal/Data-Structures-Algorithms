b6f57a12eba64d381b457c0a67562c33ebc8a158
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-03-01 13:16:17 +0530

    Time: 1 ms (96.37%) | Memory: 43.7 MB (56.58%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -1,6 +1,11 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
         int n = nums.length;
+
+        // Brute 3 pass
+        //
+
+        // Better 2 pass
         // for (int i = 0; i < n - 1; i++) {
         // if (nums[i] == nums[i + 1]) {
         // nums[i] *= 2;
@@ -8,60 +13,36 @@ class Solution {
         // }
         // }
 
-        // int i = 0;
-        // for (int j = 0; j < n; j++) {
+        // int i = 0, j = 0;
+        // while (j < n) {
         // if (nums[j] != 0) {
-        // nums[i] = nums[j];
+        // int temp = nums[j];
+        // nums[j] = nums[i];
+        // nums[i] = temp;
         // i++;
         // }
-        // }
-
-        // for (int j = i; j < n; j++) {
-        // nums[j] = 0;
+        // j++;
         // }
         // return nums;
 
-        // Better
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] == nums[i + 1]){
+        // Optimal
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (i + 1 < n && nums[i] == nums[i + 1] && nums[i] != 0) {
                 nums[i] *= 2;
                 nums[i + 1] = 0;
             }
-        }
 
-        int i = 0, j = 0;
-        while(j < n){
-            if(nums[j] != 0){
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
-                i++;
+            if (nums[i] != 0) {
+                if (i != j) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+                j++;
             }
-            j++;
         }
-         return nums;
 
-        // Optimal
-        // boolean fl = false;
-        // for (int i = 0; i < n - 1; i++) {
-        //     if (nums[i] == nums[i + 1] && nums[i] != 0) {
-        //         fl = true;
-        //         nums[i] *= 2;
-        //         nums[i + 1] = 0;
-        //     }
-
-        //     int j = i + 2;
-        //     while (j < n && nums[j] != 0) {
-        //         if (fl) {
-        //             int temp = nums[j];
-        //             nums[j] = nums[j - 1];
-        //             nums[j - 1] = temp;
-        //         }
-        //         j++;
-        //     }
-        //     fl = false;
-        // }
-
-        // return nums;
+        return nums;
     }
 }
