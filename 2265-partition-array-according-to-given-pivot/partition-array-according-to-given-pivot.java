3572f37ac7c660175173b1704b1c76491db01834
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-03-03 12:37:44 +0530

    Time: 5 ms (85.39%) | Memory: 68.1 MB (35.78%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,36 +1,59 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-       int n = nums.length;
-       int ans[] = new int[n];
-       List<Integer> lessThan = new ArrayList<>();
-       List<Integer> greaterThan = new ArrayList<>();
-       int cnt = 0;
-       for(int i = 0;i < n;i++){
-            if(nums[i] < pivot){
-                lessThan.add(nums[i]);
-            }else if(nums[i] > pivot){
-                greaterThan.add(nums[i]);
-            }else{
-                cnt++;
+        int n = nums.length;
+        // int ans[] = new int[n];
+        // List<Integer> lessThan = new ArrayList<>();
+        // List<Integer> greaterThan = new ArrayList<>();
+        // int cnt = 0;
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] < pivot) {
+        //         lessThan.add(nums[i]);
+        //     } else if (nums[i] > pivot) {
+        //         greaterThan.add(nums[i]);
+        //     } else {
+        //         cnt++;
+        //     }
+        // }
+
+        // int k = 0;
+        // for (int i = 0; i < lessThan.size(); i++) {
+        //     ans[k++] = lessThan.get(i);
+        // }
+
+        // for (int i = 0; i < cnt; i++) {
+        //     ans[k++] = pivot;
+        // }
+
+        // for (int i = 0; i < greaterThan.size(); i++) {
+        //     ans[k++] = greaterThan.get(i);
+        // }
+
+        // return ans;
+
+        // Better
+        int[] res = new int[n];
+        int less = 0, equal = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < pivot) {
+                less++;
+            } else if (nums[i] == pivot) {
+                equal++;
             }
-       }
+        }
 
-        int k = 0;
-       for(int i = 0;i < lessThan.size(); i++){
-            ans[k++] = lessThan.get(i);
-       }
+        int greater = less + equal;
+        int lessCnt = 0, equalCnt = less;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < pivot) {
+                res[lessCnt++] = nums[i];
+            } else if (nums[i] == pivot) {
+                res[equalCnt++] = pivot;
+            } else {
+                res[greater++] = nums[i];
+            }
+        }
 
-       for(int i = 0;i < cnt; i++){
-            ans[k++] = pivot;
-       }
-       
-
-       for(int i = 0;i < greaterThan.size(); i++){
-            ans[k++] = greaterThan.get(i);
-       }
-
-       return ans;
-
+        return res;
 
     }
 }
