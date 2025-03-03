7096ab8ebe1c68358b12301319c7fae58d6f521e
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-03-03 13:01:24 +0530

    Time: 5 ms (85.39%) | Memory: 68.9 MB (5.53%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -6,54 +6,80 @@ class Solution {
         // List<Integer> greaterThan = new ArrayList<>();
         // int cnt = 0;
         // for (int i = 0; i < n; i++) {
-        //     if (nums[i] < pivot) {
-        //         lessThan.add(nums[i]);
-        //     } else if (nums[i] > pivot) {
-        //         greaterThan.add(nums[i]);
-        //     } else {
-        //         cnt++;
-        //     }
+        // if (nums[i] < pivot) {
+        // lessThan.add(nums[i]);
+        // } else if (nums[i] > pivot) {
+        // greaterThan.add(nums[i]);
+        // } else {
+        // cnt++;
+        // }
         // }
 
         // int k = 0;
         // for (int i = 0; i < lessThan.size(); i++) {
-        //     ans[k++] = lessThan.get(i);
+        // ans[k++] = lessThan.get(i);
         // }
 
         // for (int i = 0; i < cnt; i++) {
-        //     ans[k++] = pivot;
+        // ans[k++] = pivot;
         // }
 
         // for (int i = 0; i < greaterThan.size(); i++) {
-        //     ans[k++] = greaterThan.get(i);
+        // ans[k++] = greaterThan.get(i);
         // }
 
         // return ans;
 
         // Better
-        int[] res = new int[n];
-        int less = 0, equal = 0;
-        for (int i = 0; i < n; i++) {
+        // int[] res = new int[n];
+        // int less = 0, equal = 0;
+        // for (int i = 0; i < n; i++) {
+        // if (nums[i] < pivot) {
+        // less++;
+        // } else if (nums[i] == pivot) {
+        // equal++;
+        // }
+        // }
+
+        // int greater = less + equal;
+        // int lessCnt = 0, equalCnt = less;
+        // for (int i = 0; i < n; i++) {
+        // if (nums[i] < pivot) {
+        // res[lessCnt++] = nums[i];
+        // } else if (nums[i] == pivot) {
+        // res[equalCnt++] = pivot;
+        // } else {
+        // res[greater++] = nums[i];
+        // }
+        // }
+
+        // return res;
+
+        // Optimal
+        int[] res1 = new int[n];
+        int less = 0, greater = n - 1;
+        int i = 0, j = n - 1;
+        while (i < n && j >= 0) {
             if (nums[i] < pivot) {
-                less++;
-            } else if (nums[i] == pivot) {
-                equal++;
+                res1[less++] = nums[i++];
+            } 
+            else{
+                i++;
+            }
+
+            if (nums[j] > pivot) {
+                res1[greater--] = nums[j--];
+            } 
+            else {
+                j--;
             }
         }
 
-        int greater = less + equal;
-        int lessCnt = 0, equalCnt = less;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] < pivot) {
-                res[lessCnt++] = nums[i];
-            } else if (nums[i] == pivot) {
-                res[equalCnt++] = pivot;
-            } else {
-                res[greater++] = nums[i];
-            }
+        while (less <= greater) {
+            res1[less++] = pivot;
         }
 
-        return res;
+        return res1;
 
     }
 }
