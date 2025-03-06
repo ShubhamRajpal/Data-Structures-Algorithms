cc2435b521c65917768580e2c648cb58fef3525d
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-03-06 11:57:08 +0530

    Time: 1 ms (100.00%) | Memory: 45.5 MB (80.11%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,26 +1,48 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        
-        int n = grid.length;
-        int size = n*n;
-        int originalSum = (size * (size+1))/2;
-        Map<Integer,Integer> freq = new HashMap<>();
-        int repeated = 0, curSum = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                freq.put(grid[i][j], freq.getOrDefault(grid[i][j],0)+1);
-                if(freq.get(grid[i][j]) == 2){
-                    repeated = grid[i][j];
-                }
 
-                curSum += grid[i][j];
+        int n = grid.length;
+        // int size = n*n;
+        // int originalSum = (size * (size+1))/2;
+        // Map<Integer,Integer> freq = new HashMap<>();
+        // int repeated = 0, curSum = 0;
+        // for(int i = 0; i < n; i++){
+        // for(int j = 0; j < n; j++){
+        // freq.put(grid[i][j], freq.getOrDefault(grid[i][j],0)+1);
+        // if(freq.get(grid[i][j]) == 2){
+        // repeated = grid[i][j];
+        // }
+
+        // curSum += grid[i][j];
+        // }
+        // }
+
+        // curSum = curSum - repeated;
+        // int missing = originalSum - curSum;
+
+        // return new int[]{repeated, missing};
+
+        // Approach 2 Using Mathematical Equation
+        long size = n * n;
+
+        long gridSum = 0, gridSqSum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                gridSum += grid[i][j];
+                gridSqSum += (grid[i][j] * grid[i][j]);
             }
         }
 
-        curSum  = curSum - repeated;
-        int missing  = originalSum - curSum;
+        long originalSum = (size * (size + 1)) / 2;
+        long originalSqSum = (size * (size + 1) * (2 * size + 1)) / 6;
 
-        return new int[]{repeated, missing};
+        long sqDiff = gridSqSum - originalSqSum;
+        long sumDiff = gridSum - originalSum;
+
+        int repeated =  (int)(sqDiff / sumDiff + sumDiff) / 2;
+        int missing =  (int)(sqDiff / sumDiff - sumDiff) / 2;
+
+        return new int[] {repeated, missing};
 
     }
 }
