0eaa46054a6a6f09592cdf7c16b5d9e43cb2d587
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-03-06 16:09:31 +0530

    Time: 94 ms (43.37%) | Memory: 47.3 MB (37.50%) - LeetSync

---
+++ 127-word-ladder/word-ladder.java
@@ -0,0 +1,41 @@
+class Pair {
+    String first;
+    int second;
+
+    public Pair(String f, int s) {
+        first = f;
+        second = s;
+    }
+}
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(beginWord, 1));
+        Set<String> st = new HashSet<String>();
+        for (int i = 0; i < wordList.size(); i++) {
+            st.add(wordList.get(i));
+        }
+        st.remove(beginWord);
+        while (!q.isEmpty()) {
+            Pair front = q.peek();
+            String word = front.first;
+            int steps = front.second;
+            q.remove();
+            if (word.equals(endWord)) return steps;
+            for (int i = 0; i < word.length(); i++) {
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    char[] replacedCharArray = word.toCharArray();
+                    replacedCharArray[i] = ch;
+                    String replacedString = new String(replacedCharArray);
+                    if (st.contains(replacedString)) {
+                        q.add(new Pair(replacedString, steps + 1));
+                        st.remove(replacedString);
+                    }
+                }
+            }
+
+        }
+        return 0;
+    }
+}
