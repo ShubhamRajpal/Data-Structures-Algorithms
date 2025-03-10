e86c216dde0dcb3575d7c683207f8602b63f51e0
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-03-10 19:49:48 +0530

    Time: 2 ms (98.05%) | Memory: 62.9 MB (70.75%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.java
+++ 3483-alternating-groups-ii/alternating-groups-ii.java
@@ -2,33 +2,71 @@ class Solution {
     public int numberOfAlternatingGroups(int[] colors, int k) {
         
         int n = colors.length;
-        int[] arr = new int[n + k - 1];
-        int K = 0;
-        for (int i = 0; i < n; i++) {
-            arr[K++] = colors[i];
-        }
+        // int[] arr = new int[n + k - 1];
+        // int K = 0;
+        // for (int i = 0; i < n; i++) {
+        //     arr[K++] = colors[i];
+        // }
 
-        for (int i = 0; i < k-1; i++) {
-            arr[K++] = colors[i];
-        }
+        // for (int i = 0; i < k-1; i++) {
+        //     arr[K++] = colors[i];
+        // }
 
-        int i = 0, j = 1, cnt = 0;
-        while (j < arr.length) {
+        // int i = 0, j = 1, cnt = 0;
+        // while (j < arr.length) {
 
-            if (arr[j] == arr[j - 1]) {
-                i = j;
-                j++;
+        //     if (arr[j] == arr[j - 1]) {
+        //         i = j;
+        //         j++;
+        //         continue;
+        //     }
+
+        //     if (j - i + 1 == k) {
+        //         cnt++;
+        //         i++;
+        //     }
+
+        //     j++;
+        // }
+
+        // return cnt;
+
+        // Optimal
+        int prevColor = colors[0];
+        int length = 1;
+        int result = 0;
+
+        for(int i = 1; i < n; i++){
+            if(prevColor == colors[i]){
+                length = 1;
+                prevColor = colors[i];
                 continue;
             }
 
-            if (j - i + 1 == k) {
-                cnt++;
-                i++;
-            }
+            length += 1;
+            prevColor = colors[i];
 
-            j++;
+            if(length >= k){
+                result++;
+            }
+ 
         }
 
-        return cnt;
+        for(int i = 0; i < k-1; i++){
+            if(prevColor == colors[i]){
+                length = 1;
+                prevColor = colors[i];
+                continue;
+            }
+
+            length += 1;
+            prevColor = colors[i];
+
+            if(length >= k){
+                result++;
+            }
+        }
+
+        return result;
     }
 }
