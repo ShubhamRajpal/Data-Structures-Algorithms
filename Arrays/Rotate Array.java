6467725f09ec8cd82c40e8912b7b85fd2ddda3de
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-04-19 12:44:32 +0530

    Update Rotate Array.java

--- Arrays/Rotate Array.java
+++ Arrays/Rotate Array.java
@@ -49,11 +49,11 @@ class Solution {
 
 
         /*
-        	Just for knowledge
-        	//left rotate
+        Just for knowledge
+          //left rotate
           reverse(nums, 0, k);
           reverse(nums, k+1, n-1);
           reverse(nums, 0, n-1);
-        	*/
+        */
     }
 }
