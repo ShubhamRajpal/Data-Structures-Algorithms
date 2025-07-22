43f8c15ea53213c6115b3a6858266b7de3ffc45d
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-07-22 13:58:32 +0530

    Update Maximum Erasure Value.java

--- Sliding Window/Maximum Erasure Value.java
+++ Sliding Window/Maximum Erasure Value.java
@@ -34,3 +34,37 @@ class Solution {
 
     }
 }
+
+
+//Approach-2 (Using cumulative sum to find subarray sum)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> indexMap = new HashMap<>();
+
+        int[] prefixSum = new int[n];
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
+        
+        int i = 0, j = 0;
+        int maxScore = 0;
+        while (j < n) {
+            if (indexMap.containsKey(nums[j])) {
+                i = Math.max(i, indexMap.get(nums[j]) + 1);
+            }
+
+            indexMap.put(nums[j], j);
+            int sum = (i == 0) ? prefixSum[j] : prefixSum[j] - prefixSum[i - 1];
+            maxScore = Math.max(maxScore, sum);
+            j++;
+
+        }
+
+        return maxScore;
+
+    }
+}
