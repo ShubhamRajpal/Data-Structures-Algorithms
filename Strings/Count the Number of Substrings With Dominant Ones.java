a94a5fc208e0021f0ec201a3b7e5a20061792ec7
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2025-11-15 20:45:52 +0530

    Create Count the Number of Substrings With Dominant Ones.java

---
+++ Strings/Count the Number of Substrings With Dominant Ones.java
@@ -0,0 +1,64 @@
+/*
+  Problem Link: https://leetcode.com/problems/count-the-number-of-substrings-with-dominant-ones/
+  Comments: Try Solving using sliding window
+*/
+
+
+//Approach (Improving the brute force by skipping index j wherever possible)
+//T.C : Worst case O(n^2), but since we skip indices we get O(n*sqrt(n))
+//S.C : O(n)
+class Solution {
+    public int numberOfSubstrings(String s) {
+
+        int n = s.length();
+        int[] cumCountOne = new int[n];
+
+        // Build prefix sum of ones
+        cumCountOne[0] = (s.charAt(0) == '1') ? 1 : 0;
+        for (int i = 1; i < n; i++) {
+            cumCountOne[i] = cumCountOne[i - 1] + (s.charAt(i) == '1' ? 1 : 0);
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i; j < n; j++) {
+
+                int oneCount = cumCountOne[j] - (i > 0 ? cumCountOne[i - 1] : 0);
+                int zeroCount = (j - i + 1) - oneCount;
+
+                int z2 = zeroCount * zeroCount;
+
+                if (z2 > oneCount) {
+                    // Skip several j's
+                    int waste = z2 - oneCount;
+                    j += waste - 1;
+
+                } else if (z2 == oneCount) {
+                    result++;
+
+                } else { //(zeroCount * zeroCount) < oneCount
+                    //[i....j] is a valid substring
+                    result++;
+
+                    //try to see how much j can shift to right until substring remains dominant
+                    int k = (int) Math.sqrt(oneCount) - zeroCount;
+                    int next = j + k;
+
+                    // out of bound, means all indices are valid
+                    if (next >= n) {  
+                        result += (n - j - 1);
+                        break;
+                    } else {
+                        result += k;
+                    }
+
+                    j = next;
+                }
+            }
+        }
+
+        return result;
+    }
+}
