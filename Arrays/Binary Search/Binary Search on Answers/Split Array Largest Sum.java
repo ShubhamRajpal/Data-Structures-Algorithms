be1cb80560dd6c7b70036394fca3e4ade1472bf5
Shubham Rajpal <88403275+ShubhamRajpal@users.noreply.github.com>
2026-03-06 21:42:01 +0530

    Create Split Array Largest Sum.java

---
+++ Arrays/Binary Search/Binary Search on Answers/Split Array Largest Sum.java
@@ -0,0 +1,49 @@
+/*
+  Problem Link: https://leetcode.com/problems/split-array-largest-sum/
+  Similar Problem - Allocate Minimum Pages - https://www.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1
+*/
+
+// Approach (binary search on answers min(max))
+// T.C: O(n log(sum - max))
+// S.C: O(1)
+class Solution {
+
+    public boolean canSplit(int midSum, int[] arr, int k) {
+        int n = arr.length;
+        int curSubSum = 0, subarraysCnt = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (curSubSum + arr[i] <= midSum) {
+                curSubSum += arr[i];
+            } else {
+                subarraysCnt++;
+                curSubSum = arr[i];
+            }
+        }
+
+        return subarraysCnt <= k;
+    }
+
+    public int splitArray(int[] nums, int k) {
+        int n = nums.length;
+
+        if (n < k)
+            return -1;
+
+        int low = Arrays.stream(nums).max().getAsInt();
+        int high = Arrays.stream(nums).sum();
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (canSplit(mid, nums, k)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+}
